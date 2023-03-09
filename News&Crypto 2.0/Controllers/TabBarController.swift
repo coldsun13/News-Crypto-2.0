141bf2f47a09c7e02dc04e818646257e93283ec7
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2023-03-10 01:21:56 +0300

    initial commit

---
+++ News&Crypto 2.0/Controllers/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News&Crypto 2.0
+//
+//  Created by Игорь Тимофеев on 10.03.23.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
