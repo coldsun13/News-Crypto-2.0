acfef20eb80987eb14a761ae982130105bc2ecc8
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2023-05-06 10:24:45 +0300

    add API-file for DataFetcherService

--- News&Crypto 2.0/Networking/DataFetcherService.swift
+++ News&Crypto 2.0/Networking/DataFetcherService.swift
@@ -2,17 +2,6 @@ import Foundation
 
 class DataFetcherService {
     
-    private enum API {
-        static let news = "https://finnhub.io/api/v1/"
-        static let coins = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
-    }
-    
-    private enum Endpoint {
-        static let search = "search?q="
-        static let topStories = "news?category=general&"
-        static let apiKey = "&token=c9m61diad3i9qg80n940"
-    }
-
     var dataFetcher: DataFetcher
     
     init(dataFetcher: DataFetcher = NetworkDataFetcher()) {
@@ -20,7 +9,7 @@ class DataFetcherService {
     }
     
     func getNews(completion: @escaping ([NewsModel]?) -> Void) {
-        let urlNews = API.news + Endpoint.topStories + Endpoint.apiKey
+        let urlNews = API.news + API.topStories + API.apiKey
         dataFetcher.fetchGenericData(urlString: urlNews, response: completion)
     }
     
