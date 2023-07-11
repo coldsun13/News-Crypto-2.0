bd297f1bc64c66696cf3bbe2c8748c32a443b1b3
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2023-07-11 19:46:19 +0300

    added delegate protocols for HeaderView and AccountView and added textField's and buttons for AccountView

--- News&Crypto 2.0/Modules/Settings/Views/HeaderView.swift
+++ News&Crypto 2.0/Modules/Settings/Views/HeaderView.swift
@@ -1,13 +1,19 @@
 import UIKit
 
+protocol HeaderViewDelegate: AnyObject {
+    func revealAlertController()
+    func changePhotoAvatarImage()
+}
+
 final class HeaderView: UIView {
     
     private let headerStackView = UIStackView()
-    
     private let nameLabel = UILabel()
     private let emailLabel = UILabel()
-    private let avatarImage = UIImageView()
+    private let avatarImageView = UIImageView()
     private let changeInfoButton = UIButton()
+    
+    weak var delegate: HeaderViewDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,7 +31,7 @@ private extension HeaderView {
     
     func addSubviews() {
         addSubviewsAndMask(headerStackView)
-        headerStackView.addAllArrangedSubviews(avatarImage,
+        headerStackView.addAllArrangedSubviews(avatarImageView,
                                                nameLabel,
                                                emailLabel,
                                                changeInfoButton)
@@ -37,24 +43,34 @@ private extension HeaderView {
         
         changeInfoButton.widthAnchor.constraint(equalToConstant: 130).isActive = true
         
-        avatarImage.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        avatarImage.widthAnchor.constraint(equalTo: avatarImage.heightAnchor).isActive = true
+        avatarImageView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor).isActive = true
     }
     
     func configureAppearance() {
         backgroundColor = .white
-        headerStackView.axis = .vertical
-        headerStackView.spacing = 15
-        headerStackView.distribution = .equalSpacing
-        headerStackView.alignment = .center
+        setupHeaderStackView()
+        setupChangeInfoButton()
+        setupAvatarImageView()
+        setupLabels()
+    }
+    
+    func setupLabels() {
         nameLabel.text = "Ihar Tsimafeyeu"
         nameLabel.font = .montserrat(20, .semibold)
         emailLabel.text = "coldsun@email.com"
         emailLabel.font = .montserrat(14, .medium)
         emailLabel.textColor = .gray
-        avatarImage.image = Resources.Images.avatarImage
-        avatarImage.tintColor = .systemGray3
-        avatarImage.layer.cornerRadius = avatarImage.frame.height / 2
+    }
+    
+    func setupHeaderStackView() {
+        headerStackView.axis = .vertical
+        headerStackView.spacing = 15
+        headerStackView.distribution = .equalSpacing
+        headerStackView.alignment = .center
+    }
+    
+    func setupChangeInfoButton() {
         changeInfoButton.backgroundColor = .blue
         changeInfoButton.layer.cornerRadius = 10
         changeInfoButton.setTitle("Edit profile", for: .normal)
@@ -62,7 +78,20 @@ private extension HeaderView {
         changeInfoButton.addTarget(self, action: #selector(changeInfoAction), for: .touchUpInside)
     }
     
+    func setupAvatarImageView() {
+        avatarImageView.image = Resources.Images.avatarImage
+        avatarImageView.tintColor = .systemGray3
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.height / 2
+        let tap = UITapGestureRecognizer(target: self, action: #selector(changePhotoAvatarAction))
+        avatarImageView.isUserInteractionEnabled = true
+        avatarImageView.addGestureRecognizer(tap)
+    }
+    
     @objc func changeInfoAction() {
-        
+        delegate?.revealAlertController()
+    }
+    
+    @objc func changePhotoAvatarAction() {
+        delegate?.changePhotoAvatarImage()
     }
 }
