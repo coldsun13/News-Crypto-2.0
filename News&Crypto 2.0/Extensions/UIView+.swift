75ed47dac7b1a55ef867157b5bec1acc52ff5f45
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2023-07-11 19:40:56 +0300

    refactor method addShadow for UIView+

--- News&Crypto 2.0/Extensions/UIView+.swift
+++ News&Crypto 2.0/Extensions/UIView+.swift
@@ -16,9 +16,9 @@ extension UIView {
     }
     
     func addShadow() {
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowRadius = 15
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 15
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
     }
 }
