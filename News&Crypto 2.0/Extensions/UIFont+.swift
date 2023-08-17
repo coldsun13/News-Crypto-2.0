aa52e44723733d0a33ee391d13c8a238d20355a9
ColdSun <coldsun1395@gmail.com>
2023-08-17 15:05:36 +0300

    refactoring

--- News&Crypto 2.0/Extensions/UIFont+.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIFont {
-    
-    static func montserrat(_ size: CGFloat, _ weight: Resources.Strings.FontWeight) -> UIFont {
-        UIFont(name: weight.rawValue, size: size)!
-    }
-}
-
