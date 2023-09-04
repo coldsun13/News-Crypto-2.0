2afdc5b9f6135eb77082ac2c592e9d4f50bd80f3
ColdSun <coldsun1395@gmail.com>
2023-09-04 23:13:21 +0300

    Create UILIb for the project

--- UILib/Sources/UILib/String+.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension String {
-    public static func stringDate(from timeInterval: TimeInterval) -> String {
-        let date = Date(timeIntervalSince1970: timeInterval)
-        return DateFormatter.prettyDateFormatter.string(from: date)
-    }
-}
